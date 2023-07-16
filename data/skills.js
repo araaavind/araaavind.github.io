5d9a9c8bcee80f2ad8ee765d98a0f7f34c8ce664
aravindmathradan <aravindmathradan2@gmail.com>
2023-07-16 23:33:05 +0530

    change in skills display

---
+++ data/skills.js
@@ -0,0 +1,193 @@
+const skills = {
+  "Languages": {
+    "duration": "30s",
+    "data": [
+      {
+        "name": "JS (ES6+)",
+        "icon": "devicon-javascript-plain"
+      },
+      {
+        "name": "TS",
+        "icon": "devicon-typescript-plain"
+      },
+      {
+        "name": "dart",
+        "icon": "devicon-dart-plain"
+      },
+      {
+        "name": "C++",
+        "icon": "devicon-cplusplus-plain"
+      },
+      {
+        "name": "python",
+        "icon": "devicon-python-plain"
+      },
+      {
+        "name": "C",
+        "icon": "devicon-c-plain"
+      },
+      {
+        "name": "CSharp",
+        "icon": "devicon-csharp-plain"
+      },
+      {
+        "name": "Java",
+        "icon": "devicon-java-plain"
+      }
+    ]
+  },
+  "Web 2.0": {
+    "duration": "60s",
+    "data": [
+      {
+        "name": "NodeJs",
+        "icon": "devicon-nodejs-plain"
+      },
+      {
+        "name": "Loopback",
+        "icon": "devicon-javascript-plain"
+      },
+      {
+        "name": "NestJs",
+        "icon": "devicon-nestjs-plain"
+      },
+      {
+        "name": "Flutter",
+        "icon": "devicon-flutter-plain"
+      },
+      {
+        "name": "ReactJs",
+        "icon": "devicon-react-original"
+      },
+      {
+        "name": "AngularJs",
+        "icon": "devicon-angularjs-plain"
+      },
+      {
+        "name": "Electron",
+        "icon": "devicon-electron-original"
+      },
+      {
+        "name": "Redis",
+        "icon": "devicon-redis-plain"
+      },
+      {
+        "name": "Docker",
+        "icon": "devicon-docker-plain"
+      },
+      {
+        "name": "NGINX",
+        "icon": "devicon-nginx-original"
+      }
+    ]
+  },
+  "Web 3.0": {
+    "duration": "15s",
+    "data": [
+      {
+        "name": "ThreeJs",
+        "icon": "devicon-threejs-original"
+      },
+      {
+        "name": "Unity",
+        "icon": "devicon-unity-original"
+      },
+      {
+        "name": "Unreal",
+        "icon": "devicon-unrealengine-original"
+      },
+      {
+        "name": "Blockchain",
+        "icon": "'fas fa-coins'"
+      }
+    ]
+  },
+  "ML/DL Libs": {
+    "duration": "40s",
+    "data": [
+      {
+        "name": "Numpy",
+        "icon": "devicon-numpy-plain"
+      },
+      {
+        "name": "Pandas",
+        "icon": "devicon-pandas-plain"
+      },
+      {
+        "name": "Scikit-learn",
+        "icon": "devicon-python-plain"
+      },
+      {
+        "name": "Octave",
+        "icon": "devicon-matlab-plain"
+      }
+    ]
+  },
+  "Database": {
+    "duration": "25s",
+    "data": [
+      {
+        "name": "Oracle",
+        "icon": "devicon-oracle-plain"
+      },
+      {
+        "name": "MySQL",
+        "icon": "devicon-mysql-plain"
+      },
+      {
+        "name": "Firebase/Firestore",
+        "icon": "devicon-firebase-plain"
+      },
+      {
+        "name": "SQLite",
+        "icon": "devicon-sqlite-plain"
+      }
+    ]
+  },
+  "Others": {
+    "duration": "55s",
+    "data": [
+      {
+        "name": "Bash",
+        "icon": "devicon-bash-plain"
+      },
+      {
+        "name": "Git",
+        "icon": "devicon-git-plain"
+      },
+      {
+        "name": "Latex",
+        "icon": "devicon-latex-plain"
+      },
+      {
+        "name": "Markdown",
+        "icon": "devicon-markdown-original"
+      }
+    ]
+  },
+  "Graphics": {
+    "duration": "40s",
+    "data": [
+      {
+        "name": "Blender",
+        "icon": "devicon-blender-original"
+      },
+      {
+        "name": "Illustrator",
+        "icon": "devicon-illustrator-plain"
+      },
+      {
+        "name": "Figma",
+        "icon": "devicon-figma-plain"
+      },
+      {
+        "name": "Photoshop",
+        "icon": "devicon-photoshop-plain"
+      },
+      {
+        "name": "Animate",
+        "icon": "'fas fa-angle-up'"
+      }
+    ]
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,26 +1,69 @@
+
+function populateSkills() {
+  for (const cat in skills) {
+    const skillContainer = $(`<div class="skill-container"></div>`);
+    const skillCategory = $(`<div class="skill-category"><span class="category-text">${cat}</span></div>`);
+    skillContainer.append(skillCategory);
+    let skillStrip = $(`<div class="skill-strip"></div>`);
+    let marqueeContainer = $(`<div class="marquee-container"></div>`);
+    let marqueeFirst = $(`<div class="marquee first" style="animation: marquee-first ${skills[cat].duration} linear infinite"></div>`);
+    let marqueeSecond = $(`<div class="marquee second" style="animation: marquee-second ${skills[cat].duration} linear infinite"></div>`);
+    skillContainer.append(skillStrip);
+    skillStrip.append(marqueeContainer);
+    marqueeContainer.append(marqueeFirst);
+    marqueeContainer.append(marqueeSecond);
+
+    skills[cat].data.forEach((skill, index) => {
+      if ($(window).width() < 1400 && index != 0 && index % 4 == 0) {
+        // skillStrip = $(`<div class="skill-strip"></div>`);
+        marqueeContainer = $(`<div class="marquee-container"></div>`);
+        marqueeFirst = $(`<div class="marquee first" style="animation: marquee-first ${skills[cat].duration} linear infinite"></div>`);
+        marqueeSecond = $(`<div class="marquee second" style="animation: marquee-second ${skills[cat].duration} linear infinite"></div>`);
+        // skillContainer.append(skillStrip);
+        skillStrip.append(marqueeContainer);
+        marqueeContainer.append(marqueeFirst);
+        marqueeContainer.append(marqueeSecond);
+      } else if ($(window).width() < 400 && index != 0 && index % 2 == 0) {
+        skillStrip = $(`<div class="skill-strip"></div>`);
+        marqueeContainer = $(`<div class="marquee-container"></div>`);
+        marqueeFirst = $(`<div class="marquee first" style="animation: marquee-first ${skills[cat].duration} linear infinite"></div>`);
+        marqueeSecond = $(`<div class="marquee second" style="animation: marquee-second ${skills[cat].duration} linear infinite"></div>`);
+        skillContainer.append(skillStrip);
+        skillStrip.append(marqueeContainer);
+        marqueeContainer.append(marqueeFirst);
+        marqueeContainer.append(marqueeSecond);
+      }
+      marqueeFirst.append($(`<span class=skill-span><i class=${skill.icon}></i>${skill.name}</span>`));
+      marqueeSecond.append($(`<span class=skill-span><i class=${skill.icon}></i>${skill.name}</span>`));
+    });
+    $('.skills').children('.max-width').append(skillContainer);
+  }
+}
+populateSkills();
+
 $(document).ready(() => {
-    $(window).scroll(() => {
-        if (this.scrollY > 20) {
-            $('.navbar').addClass("sticky");
-            $('.logo').css('visibility', "visible");
-        } else {
-            $('.navbar').removeClass("sticky");
-            $('.logo').css('visibility', "hidden");
-        }
+  $(window).scroll(() => {
+    if (this.scrollY > 20) {
+      $('.navbar').addClass("sticky");
+      $('.logo').css('visibility', "visible");
+    } else {
+      $('.navbar').removeClass("sticky");
+      $('.logo').css('visibility', "hidden");
+    }
 
-        if (this.scrollY > 500) {
-            $('.scroll-up-btn').addClass("show");
-        } else {
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+    if (this.scrollY > 500) {
+      $('.scroll-up-btn').addClass("show");
+    } else {
+      $('.scroll-up-btn').removeClass("show");
+    }
+  });
 
-    $('.scroll-up-btn').click(() => {
-        $('html').animate({ scrollTop: 0 });
-    });
+  $('.scroll-up-btn').click(() => {
+    $('html').animate({ scrollTop: 0 });
+  });
 
-    $('.menu-btn').click(() => {
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+  $('.menu-btn').click(() => {
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+  });
 });
