a0d8fe49daed1e625513ccf9d4327c0903b0a425
Aravind Unnikrishnan <aravindmathradan2@gmail.com>
2023-07-19 12:03:19 +0530

    added urls

--- data/skills.js
+++ data/skills.js
@@ -4,35 +4,43 @@ const skills = {
     "data": [
       {
         "name": "JS (ES6+)",
-        "icon": "devicon-javascript-plain"
+        "icon": "devicon-javascript-plain",
+        "url": "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
       },
       {
         "name": "TS",
-        "icon": "devicon-typescript-plain"
+        "icon": "devicon-typescript-plain",
+        "url": "https://www.typescriptlang.org/"
       },
       {
         "name": "dart",
-        "icon": "devicon-dart-plain"
+        "icon": "devicon-dart-plain",
+        "url": "https://dart.dev/"
       },
       {
         "name": "C++",
-        "icon": "devicon-cplusplus-plain"
+        "icon": "devicon-cplusplus-plain",
+        "url": "https://en.wikipedia.org/wiki/C%2B%2B"
       },
       {
         "name": "python",
-        "icon": "devicon-python-plain"
+        "icon": "devicon-python-plain",
+        "url": "https://www.python.org/"
       },
       {
         "name": "C",
-        "icon": "devicon-c-plain"
+        "icon": "devicon-c-plain",
+        "url": "https://en.wikipedia.org/wiki/C_(programming_language)"
       },
       {
         "name": "CSharp",
-        "icon": "devicon-csharp-plain"
+        "icon": "devicon-csharp-plain",
+        "url": "https://en.wikipedia.org/wiki/C_Sharp_(programming_language)"
       },
       {
         "name": "Java",
-        "icon": "devicon-java-plain"
+        "icon": "devicon-java-plain",
+        "url": "https://www.java.com/en/"
       }
     ]
   },
@@ -41,43 +49,53 @@ const skills = {
     "data": [
       {
         "name": "NodeJs",
-        "icon": "devicon-nodejs-plain"
+        "icon": "devicon-nodejs-plain",
+        "url": "https://nodejs.org/en"
       },
       {
         "name": "Loopback",
-        "icon": "devicon-javascript-plain"
+        "icon": "devicon-javascript-plain",
+        "url": "https://loopback.io/"
       },
       {
         "name": "NestJs",
-        "icon": "devicon-nestjs-plain"
+        "icon": "devicon-nestjs-plain",
+        "url": "https://nestjs.com/"
       },
       {
         "name": "Flutter",
-        "icon": "devicon-flutter-plain"
+        "icon": "devicon-flutter-plain",
+        "url": "https://flutter.dev/"
       },
       {
         "name": "ReactJs",
-        "icon": "devicon-react-original"
+        "icon": "devicon-react-original",
+        "url": "https://react.dev/"
       },
       {
         "name": "AngularJs",
-        "icon": "devicon-angularjs-plain"
+        "icon": "devicon-angularjs-plain",
+        "url": "https://angularjs.org/"
       },
       {
         "name": "Electron",
-        "icon": "devicon-electron-original"
+        "icon": "devicon-electron-original",
+        "url": "https://www.electronjs.org/"
       },
       {
         "name": "Redis",
-        "icon": "devicon-redis-plain"
+        "icon": "devicon-redis-plain",
+        "url": "https://redis.io/"
       },
       {
         "name": "Docker",
-        "icon": "devicon-docker-plain"
+        "icon": "devicon-docker-plain",
+        "url": "https://www.docker.com/"
       },
       {
         "name": "NGINX",
-        "icon": "devicon-nginx-original"
+        "icon": "devicon-nginx-original",
+        "url": "https://www.nginx.com/"
       }
     ]
   },
@@ -86,19 +104,23 @@ const skills = {
     "data": [
       {
         "name": "ThreeJs",
-        "icon": "devicon-threejs-original"
+        "icon": "devicon-threejs-original",
+        "url": "https://threejs.org/"
       },
       {
         "name": "Unity",
-        "icon": "devicon-unity-original"
+        "icon": "devicon-unity-original",
+        "url": "https://unity.com/"
       },
       {
         "name": "Unreal",
-        "icon": "devicon-unrealengine-original"
+        "icon": "devicon-unrealengine-original",
+        "url": "https://www.unrealengine.com/en-US"
       },
       {
         "name": "Blockchain",
-        "icon": "'fas fa-coins'"
+        "icon": "'fas fa-coins'",
+        "url": "https://en.wikipedia.org/wiki/Blockchain.com"
       }
     ]
   },
@@ -107,19 +129,23 @@ const skills = {
     "data": [
       {
         "name": "Numpy",
-        "icon": "devicon-numpy-plain"
+        "icon": "devicon-numpy-plain",
+        "url": "https://numpy.org/"
       },
       {
         "name": "Pandas",
-        "icon": "devicon-pandas-plain"
+        "icon": "devicon-pandas-plain",
+        "url": "https://pandas.pydata.org/"
       },
       {
         "name": "Scikit-learn",
-        "icon": "devicon-python-plain"
+        "icon": "devicon-python-plain",
+        "url": "https://scikit-learn.org/stable/"
       },
       {
         "name": "Octave",
-        "icon": "devicon-matlab-plain"
+        "icon": "devicon-matlab-plain",
+        "url": "https://octave.org/"
       }
     ]
   },
@@ -128,19 +154,23 @@ const skills = {
     "data": [
       {
         "name": "Oracle",
-        "icon": "devicon-oracle-plain"
+        "icon": "devicon-oracle-plain",
+        "url": "https://en.wikipedia.org/wiki/Oracle_Database"
       },
       {
         "name": "MySQL",
-        "icon": "devicon-mysql-plain"
+        "icon": "devicon-mysql-plain",
+        "url": "https://www.mysql.com/"
       },
       {
         "name": "Firebase/Firestore",
-        "icon": "devicon-firebase-plain"
+        "icon": "devicon-firebase-plain",
+        "url": "https://firebase.google.com/"
       },
       {
         "name": "SQLite",
-        "icon": "devicon-sqlite-plain"
+        "icon": "devicon-sqlite-plain",
+        "url": "https://www.sqlite.org/index.html"
       }
     ]
   },
@@ -149,19 +179,23 @@ const skills = {
     "data": [
       {
         "name": "Bash",
-        "icon": "devicon-bash-plain"
+        "icon": "devicon-bash-plain",
+        "url": "https://www.gnu.org/software/bash/"
       },
       {
         "name": "Git",
-        "icon": "devicon-git-plain"
+        "icon": "devicon-git-plain",
+        "url": "https://git-scm.com/"
       },
       {
         "name": "Latex",
-        "icon": "devicon-latex-plain"
+        "icon": "devicon-latex-plain",
+        "url": "https://www.latex-project.org/"
       },
       {
         "name": "Markdown",
-        "icon": "devicon-markdown-original"
+        "icon": "devicon-markdown-original",
+        "url": "https://www.markdownguide.org/"
       }
     ]
   },
@@ -170,23 +204,28 @@ const skills = {
     "data": [
       {
         "name": "Blender",
-        "icon": "devicon-blender-original"
+        "icon": "devicon-blender-original",
+        "url": "https://www.blender.org/"
       },
       {
         "name": "Illustrator",
-        "icon": "devicon-illustrator-plain"
+        "icon": "devicon-illustrator-plain",
+        "url": "https://www.adobe.com/in/products/illustrator.html"
       },
       {
         "name": "Figma",
-        "icon": "devicon-figma-plain"
+        "icon": "devicon-figma-plain",
+        "url": "https://www.figma.com/"
       },
       {
         "name": "Photoshop",
-        "icon": "devicon-photoshop-plain"
+        "icon": "devicon-photoshop-plain",
+        "url": "https://www.adobe.com/in/products/photoshop.html"
       },
       {
         "name": "Animate",
-        "icon": "'fas fa-angle-up'"
+        "icon": "'fas fa-angle-up'",
+        "url": "https://www.adobe.com/in/products/animate.html"
       }
     ]
   }
